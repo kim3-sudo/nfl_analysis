8a24ad665e32a72a06f199f08fb77415943c64fa
Sejin Kim <kim3@kenyon.edu>
2021-02-09 21:35:36 -0500

    Added new script wireframes

---
+++ src/analysis_script.R
@@ -0,0 +1,7 @@
+### NFL Data Analysis
+### Josh Katz & Sejin Kim
+### STAT 306 S21 @ Kenyon College
+
+library(mosaic)
+library(ggplot2)
+library(nflfastR)
---
+++ src/plot_script.R
@@ -0,0 +1,7 @@
+### NFL Data Analysis Plot Generator
+### Josh Katz & Sejin Kim
+### STAT 306 S21 @ Kenyon College
+
+library(mosaic)
+library(ggplot2)
+library(nflfastR)
