6f1efe5d837479f86510cd6a93fdb6719aad26ad
Sejin Kim <kim3@kenyon.edu>
2021-02-14 18:09:54 +0000

    Removed bunk try/catch code

--- src/analysis_script.R
+++ src/analysis_script.R
@@ -41,35 +41,7 @@ ggplot2Result = tryCatch({
 }, finally = {
   message("Finished single library load attempt: ggplot2")
 })
-# Attempt to load nflfastR
-nflfastrResult = tryCatch({
-  message("Try to load local 2018 dataset into var `nfldata2018`")
-  nfldata2018 <- readRDS('../data/play_by_play_2018.rds')
-  message("Try to load local 2019 dataset into var `nfldata2019`")
-  nfldata2019 <- readRDS('../data/play_by_play_2019.rds')
-  message("Try to load local 2020 dataset into var `nfldata2020`")
-  nfldata2020 <- readRDS('../data/play_by_play_2020.rds')
-}, warning = function(w) {
-  warning("Possible issue trying load local data nflfastr")
-  mosaicResult = tryCatch({
-    message("Attempting to load 2018 data by remote RDS method")
-    nfldata2018 <- readRDS(url('https://raw.githubusercontent.com/guga31bb/nflfastR-data/master/data/play_by_play_2018.rds'))
-    message("Attempting to load 2019 data by remote RDS method")
-    nfldata2019 <- readRDS(url('https://raw.githubusercontent.com/guga31bb/nflfastR-data/master/data/play_by_play_2019.rds'))
-    message("Attempting to load 2020 data by remote RDS method")
-    nfldata2020 <- readRDS(url('https://raw.githubusercontent.com/guga31bb/nflfastR-data/master/data/play_by_play_2020.rds'))
-  }, warning = function(w) {
-    warning(w)
-  }, error = function(e) {
-    warning(e)
-  })
-}, error = function(e) {
-  warning(e)
-}, finally = {
-  message("Finished library family load attempt: nfldataR")
-})
-message("####################")
-message("ALL LIBRARIES LOADED")
-message("####################")
 
-head(nfldata2018)
+# Load in nflfastR
+
+# Data analysis
